5ea421c23ac541a28988bc59b6a425dd65af5771
charles <charlesclinton2003@gmail.com>
2020-04-11 17:00:33 +0100

    touched a few

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
